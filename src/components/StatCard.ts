34cd7a081ac845fa70099e9bf54a1cd21d2eabad
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-10-18 21:39:42 +0100

    Move component styles to css module

--- src/components/StatCard.ts
+++ src/components/StatCard.ts
@@ -1,21 +1,22 @@
 import { createIcon } from "./CustomIcon";
+import styles from "./StatCard.module.css";
 
 
 export default function StatCard(stat: Stat) {
 
   // Create the card
   const card = document.createElement("div");
-  card.classList.add("stat-card");
+  card.classList.add(styles["stat-card"]);
 
   card.innerHTML = `
     <h3>
       ${stat.label}
       ${createIcon("info").outerHTML}
-      <span class="tooltip">More information about ${stat.label}...</span>
+      <span class="${styles.tooltip}">More information about ${stat.label}...</span>
     </h3>
     <p>
       ${stat.value}
-      <span class="${stat.rate[0] === "+" ? "positive-rate" : "negative-rate"}">
+      <span class="${stat.rate[0] === "+" ? styles["positive-rate"] : styles["negative-rate"]}">
         ${createIcon("rate").outerHTML}
         ${stat.rate}
       </span>
