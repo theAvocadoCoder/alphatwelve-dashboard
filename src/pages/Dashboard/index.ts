56ace006a83182bc1daab8649db92f6a4d938d8f
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-10-20 21:01:28 +0100

    Markup and style Dashboard page

---
+++ src/pages/Dashboard/index.ts
@@ -0,0 +1,47 @@
+import styles from "./Dashboard.module.css";
+import StatCard,{Stat} from "../../components/StatCard";
+import { stats as statsData } from "../../mock-data.json";
+
+const sections = [
+  {
+    name: "stats",
+    title: "",
+    content: statsData.map((stat: Stat) => StatCard(stat)),
+  },
+  {
+    name: "monthly",
+    title: "Event registrations per month",
+    content: ["Chart", "Carousel"],
+  },
+  {
+    name: "history",
+    title: "Events History",
+    content: ["History..."],
+  },
+]
+
+export default function Dashboard() {
+  const container = document.createDocumentFragment();
+
+  const welcome = document.createElement("h1");
+  welcome.textContent = "Welcome, here's your summary";
+
+  const length = sections.length;
+  const sectionList:HTMLElement[] = [];
+
+  for (let i = 0; i < length; i++) {
+    const currentSection = sections[i];
+    const section = document.createElement("section");
+
+    section.classList.add(styles[currentSection.name] || currentSection.name);
+    section.innerHTML = `${currentSection.title ? `<h2>${currentSection.title}</h2>` : ""}`;
+    section.append(...currentSection.content);
+
+    sectionList.push(section);
+  }
+
+  container.append(welcome, ...sectionList);
+
+  return container;
+}
+
